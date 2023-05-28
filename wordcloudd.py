3546ad0c81df289aabfd2d62bfcec43864c6b065
SONIA RIKHI <112720079+SONERIKHI@users.noreply.github.com>
2023-05-28 14:21:11 +0530

    Add files via upload

---
+++ wordcloudd.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 28 13:33:07 2023
+
+@author: RUCHIR
+"""
+
+import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+st.image("ruchirr.jpeg")
+
+def generate_wordcloud(text):
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.tight_layout(pad=0)
+    st.pyplot()
+
+def main():
+    st.title("Word Cloud Generator")
+    st.write("Enter your text below to generate a word cloud.")
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    text = st.text_area("Text")
+
+    if st.button("Generate Word Cloud"):
+        generate_wordcloud(text)
+
+if __name__ == '__main__':
+    main()
